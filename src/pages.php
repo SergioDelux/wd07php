842ca1d1465410839fe36a2eb354d275578164b2
SergioDelux <sergik5obrspndelux@gmail.com>
2022-09-29 13:46:16 +0300

    lesson3

---
+++ src/pages.php
@@ -0,0 +1,20 @@
+<?php
+$pages = [
+    [
+        'title' => 'lolol',
+        'content'=>'fgdfghjkl'
+    ],
+    [
+        'title' => 'lolol2',
+        'content'=>'fgdfghjkl'
+    ],
+    [
+        'title' => 'lolol3',
+        'content'=>'fgdfghjkl'
+    ],
+    [
+        'title' => 'lolol4',
+        'content'=>'fgdfghjkl'
+    ],
+];
+    echo "Hello World";
